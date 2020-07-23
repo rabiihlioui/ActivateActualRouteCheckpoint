4d7ac3f3c2b6dc048857202ff69cd5bf517441e6
rabiihlioui <rabiihlioui@gmail.com>
2020-07-23 03:10:53 +0100

    Activate Actual Route Checkpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { TodosComponent } from './todos/todos.component';
+import { NewTodoComponent } from './new-todo/new-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    TodosComponent,
+    NewTodoComponent
   ],
   imports: [
     BrowserModule,
